5f98d52a5cde536b4508d882693a2dca69bcfb53
luarakelly <lua.kellly@gmail.com>
2026-09-10 23:51:17 +0300

    feat: wire new project modal into main view

--- src/main/java/com/alexandria/view/MainView.java
+++ src/main/java/com/alexandria/view/MainView.java
@@ -1,6 +1,10 @@
 package com.alexandria.view;
 
+import java.util.function.Consumer;
+
 import com.alexandria.view.components.side_navbar.SideNavbar;
+import com.alexandria.view.components.shared.modal.Modal;
+import com.alexandria.view.components.side_navbar.new_project.NewProjectModal;
 import com.alexandria.view.router.Route;
 import com.alexandria.view.router.ViewRouter;
 
@@ -8,8 +12,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 public class MainView extends StackPane {
+
     private final SideNavbar sideNavbar;
     private final ViewRouter viewRouter;
+
+    private final Modal modal = new Modal();
+    private final NewProjectModal newProjectModal = new NewProjectModal();
 
     public MainView() {
         sideNavbar = new SideNavbar();
@@ -18,23 +26,56 @@ public class MainView extends StackPane {
         BorderPane shell = new BorderPane();
         shell.setLeft(sideNavbar);
         shell.setCenter(viewRouter);
-        getChildren().add(shell);
+
+        getChildren().addAll(shell, modal);
 
         configureNavigation();
-        navigateTo(Route.LIBRARY);
+
+        viewRouter.navigateTo(Route.LIBRARY);
     }
 
     private void configureNavigation() {
-        sideNavbar.setOnNavigate(id -> navigateTo(Route.fromId(id)));
-        sideNavbar.setOnFooterNavigate(id -> navigateTo(Route.fromId(id)));
-        sideNavbar.setOnNewProject(() -> {
-            // TODO
-        });
+        sideNavbar.setOnNavigate(id -> navigateAndSync(Route.fromId(id)));
+
+        sideNavbar.setOnFooterNavigate(id -> navigateAndSync(Route.fromId(id)));
+
+        sideNavbar.setOnNewProject(this::showNewProjectModal);
     }
 
-    private void navigateTo(Route route) {
+    private void navigateAndSync(Route route) {
         viewRouter.navigateTo(route);
         sideNavbar.selectItem(route.id());
+    }
+
+    private void showNewProjectModal() {
+        newProjectModal.reset();
+        modal.show(newProjectModal);
+    }
+
+    public void closeProjectModal() {
+        modal.hide();
+    }
+
+    /* New Project */
+
+    public void setOnProjectCreated(
+            Consumer<NewProjectModal.CreatedProject> handler) {
+
+        newProjectModal.setOnCreated(handler);
+    }
+
+    public void setNewProjectLoading(boolean loading) {
+        newProjectModal.setLoading(loading);
+    }
+
+    public void showProjectError(String message) {
+        newProjectModal.showError(message);
+    }
+
+    /* Navigation */
+
+    public void navigateTo(Route route) {
+        navigateAndSync(route);
     }
 
     public SideNavbar getSideNavbar() {
